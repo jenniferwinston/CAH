c71b598282e248f77847a1c0c04ac5d2b1f48625
Kevin Whiteside 1 <kevindwhiteside@icloud.com>
2016-10-24 19:45:42 -0400

    yo

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ var mysql = require('mysql');
 var path = require('path');
 var cards = require('./assets/cards.js')
 var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
 
 require('./routing/html-routes.js')(app);
 
